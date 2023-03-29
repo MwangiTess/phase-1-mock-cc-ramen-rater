083e3c34d8eda12b55927a2e5a8d9a4d5924f6b6
Teresia <teresiamwangi26@gmail.com>
2023-03-29 20:00:03 +0300

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-const menu = document.getElementById("ramen-menu");
-const detail = document.getElementById("ramen-detail");
-const form = document.getElementById("new-ramen");
+const ramenMenu = document.getElementById("ramen-menu");
+const ramenDetail = document.getElementById("ramen-detail");
+const ramenForm = document.getElementById("new-ramen");
 fetch("http://localhost:3000/ramens")
   .then((response) => response.json())
   .then((ramens) => {
@@ -8,12 +8,12 @@ fetch("http://localhost:3000/ramens")
       const img = document.createElement("img");
       img.src = ramen.image;
       img.alt = ramen.name;
-      menu.appendChild(img);
+      ramenMenu.appendChild(img);
       img.addEventListener("click", () => {
         fetch(`http://localhost:3000/ramens/${ramen.id}`)
           .then((response) => response.json())
           .then((details) => {
-            detail.innerHTML = `
+            ramenDetail.innerHTML = `
                 <h4>${details.name}</h4>
                 <p>Restaurant: ${details.restaurant}</p>
                 <img src="${details.image}" alt="${details.name}">
@@ -24,23 +24,23 @@ fetch("http://localhost:3000/ramens")
       });
       form.addEventListener("submit", (event) => {
         event.preventDefault();
-        const nameInput = document.querySelector("#new-name");
-        const imageInput = document.querySelector("#new-image");
-        const restaurantInput = document.querySelector("#new-restaurant");
-        const ratingInput = document.querySelector("#new-rating");
-        const commentInput = document.querySelector("#new-comment");
+        const newName = document.querySelector("#new-name");
+        const newImage = document.querySelector("#new-image");
+        const newRestaurantName = document.querySelector("#new-restaurant");
+        const newRating = document.querySelector("#new-rating");
+        const newComment = document.querySelector("#new-comment");
         const newRamenName = document.createElement("h2");
         const newRamenImage = document.createElement("img");
         const newRamenRestaurant = document.createElement("p");
         const newRamenRating = document.createElement("p");
         const newRamenComment = document.createElement("p");
-        newRamenName.textContent = nameInput.value;
-        newRamenImage.src = imageInput.value;
-        newRamenRestaurant.textContent = restaurantInput.value;
-        newRamenRating.textContent = `Rating: ${ratingInput.value}`;
-        newRamenComment.textContent = `Comment: ${commentInput.value}`;
-        menu.appendChild(newRamenImage);
-        form.reset();
+        newRamenName.textContent = newName.value;
+        newRamenImage.src = newImage.value;
+        newRamenRestaurant.textContent = newRestaurantName.value;
+        newRamenRating.textContent = `Rating: ${newRating.value}`;
+        newRamenComment.textContent = `Comment: ${newComment.value}`;
+        ramenMenu.appendChild(newRamenImage);
+        ramenForm.reset();
         newRamenImage.addEventListener("click", () => {
           detail.innerHTML = `
           <h4>${newRamenName.textContent}</h4>
